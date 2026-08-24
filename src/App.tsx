f640abbd7b6ab940e7fd7ba2574d5fe4c66f0149
Katori <katorilite0220@gmail.com>
2026-08-24 08:50:30 +0700

    feat: khởi tạo phu-xuan-react với Vite + React + TypeScript + ESLint + Prettier

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div>
+      <h1>phu-xuan-react</h1>
+      <p>Dự án xuyên suốt học phần Web FrontEnd nâng cao</p>
+      <p>Trường Đại học Phú Xuân - Kh oa Công nghệ thông tin</p>
+      <p>Sinh viên: Anh</p>
+    </div>
+  )
+}
+export default App
