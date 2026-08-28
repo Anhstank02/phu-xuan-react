11a975ab2003de73651af941955708e4ba41d970
Katori <katorilite0220@gmail.com>
2026-08-28 07:20:43 +0700

    feat: hoàn thành trang chủ v0.1 Header, WelcomeBanner, PostCard, Footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
+// App.tsx trang chủ phu-xuan-react v0.1
 import Header from './components/Header'
+import WelcomeBanner from './components/WelcomeBanner'
+import PostCard from './components/PostCard'
+import Footer from './components/Footer'
 
 function App() {
   return (
-    <div>
+    <>
       <Header />
       <main>
-        <p>Nội dung chính sẽ được xây dựng ở các buổi sau.</p>
+        <WelcomeBanner />
+        <PostCard />
       </main>
-    </div>
+      <Footer />
+    </>
   )
 }
 
